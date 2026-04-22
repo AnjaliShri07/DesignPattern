26ae77e00d3614f352c607c124bae35e7483db5a
AnjaliShri07 <shri.anjali07@gmail.com>
2026-04-22 18:44:20 +0530

    added memento design pattern

--- src/com/designPattern/behavioral/Memento/MementoDemo.java
+++ src/com/designPattern/behavioral/Memento/MementoDemo.java
@@ -1,3 +1,52 @@
+// Memento: stores state
+class Memento {
+    private final String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
+
+// Originator: creates and restores mementos
+class Originator {
+    private String state;
+
+    public void setState(String state) {
+        this.state = state;
+        System.out.println("State set to: " + state);
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public Memento saveStateToMemento() {
+        return new Memento(state);
+    }
+
+    public void restoreStateFromMemento(Memento memento) {
+        state = memento.getState();
+        System.out.println("State restored to: " + state);
+    }
+}
+
+// Caretaker: manages mementos
+class Caretaker {
+    private final java.util.List<Memento> mementoList = new java.util.ArrayList<>();
+
+    public void add(Memento state) {
+        mementoList.add(state);
+    }
+
+    public Memento get(int index) {
+        return mementoList.get(index);
+    }
+}
+
 // Client
 public class MementoDemo {
     public static void main(String[] args) {
